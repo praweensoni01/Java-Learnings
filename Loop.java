678bb5f204143d6baedb3f3653d3b6ca634d7eee
praweensoni01 <ask.psoni@gmail.com>
2024-12-09 20:45:45 +0530

    Java Loops completed

---
+++ Loop.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Loop{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        // For Loop (Table Print using for loop.)
+        for(int count = 0; count < 10; count = count + 1){
+            System.out.println(n + " * " + (count + 1) + " = " + (n*(count + 1)));
+        }
+
+        //  While Loop
+
+        // int i = 1;
+        // while(i<11){
+        //     System.out.println(i);
+        //     i = i+1;
+        // }
+
+        // Do While Loop
+
+        // do{
+        //     System.out.println(i);
+        //     i=i+1;
+        // }while(i<11);
+    }
+}
