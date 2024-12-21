7aefd4c9a8c35f77a1b3f1a86ce6aa9baa9674bc
praweensoni01 <ask.psoni@gmail.com>
2024-12-21 20:35:47 +0530

    infinite loop choice using Do-while loop

---
+++ Control Statements/Question.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Question{
+    public static void main(String arg[]){
+        int choice;
+        Scanner sc= new Scanner(System.in);
+        do{
+        System.out.print("Enter 1 to input marks or 0 to stop program : ");
+        choice = sc.nextInt();
+        if(choice==1){
+            System.out.println("Enter marks (out of 100) : ");
+            int marks = sc.nextInt();
+            if(marks>=90){
+                System.out.println("Very good");
+            }
+            else if(marks>=60){
+                System.out.println("This is also good");
+            }
+            else if(marks>=0){
+                System.out.println("Not bad");
+            }
+            else {
+                System.out.println("Invalid marks plase enter a value between 0 to 100.");
+            }
+        }
+        else if(choice!=0){
+                System.out.println("Invalid choice please enter 1 to continue or 0 to stop the program.");
+            }
+        }
+        while(choice!=0);
+        System.out.println("Program stopped.");
+    }
+}
