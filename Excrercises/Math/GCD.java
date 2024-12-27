c5c8d0705fea2b32f93c6beb923e895b0eeccd39
praweensoni01 <ask.psoni@gmail.com>
2024-12-27 21:13:46 +0530

    GCD calculate in java

---
+++ Excrercises/Math/GCD.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class GCD {
+    // Function to calculate GCD of two numbers
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+
+        // Calculate and print using GCD function
+        int result = gcd(num1, num2);
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
