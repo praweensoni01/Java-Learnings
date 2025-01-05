674e1d42aaabc5e78a43e56b06f296638e19ec9f
praweensoni01 <ask.psoni@gmail.com>
2025-01-05 21:01:17 +0530

    Find Armstrong number using for loop

--- Exercise/Simple Questions/Armstrong.java
+++ Exercise/Simple Questions/Armstrong.java
@@ -1,3 +1,4 @@
+
 // Write a program to find whether the given number is Armstrong.
 // Example : 371 = 3*3 + 7*3 + 1*3 = 371 hence 371 is Armstrong number(Narcissistic Numbers).
 // Example : 153 = 1*3 + 5*3 + 3*3 = 371 hence 371 is Armstrong number(Narcissistic Numbers).
@@ -10,13 +11,14 @@ public class Armstrong {
         int number = scanner.nextInt();
         scanner.close();
 
-        if (isArmstrong(number)) {
+        // if (isArmstrong(number)) {
+        if (isArmstrong2(number)) {
             System.out.println(number + " is an Armstrong number.");
         } else {
             System.out.println(number + " is not an Armstrong number.");
         }
     }
-
+    // Using While Loop
     public static boolean isArmstrong(int number) {
         int originalNumber = number;
         int result = 0;
@@ -29,5 +31,18 @@ public class Armstrong {
         }
 
         return result == originalNumber;
+    }
+    // Using For loop
+    public static boolean isArmstrong2(int number) {
+        int sum = 0;
+        int originalNum = number;
+        int numberOfDigits = String.valueOf(number).length();
+    
+        for (int i = 0; i < numberOfDigits; i++) {
+            int digit = originalNum % 10;
+            sum += Math.pow(digit, numberOfDigits);
+            originalNum /= 10;
+        }
+        return sum == number;
     }
 }
