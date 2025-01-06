31828868a724dd0b0cb40ba35c7668d09096f76e
praweensoni01 <ask.psoni@gmail.com>
2025-01-06 14:09:17 +0530

    Minor Changes

--- Basics/Strings.java
+++ Basics/Strings.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 // Tip : Strings are Inmutable(Not changebal) in java (IF i want to modify to a string then create a new string and modify orignal string and store to new string variable.)
 
@@ -10,7 +10,7 @@ class Strings{
         String lname = "Soni";
         //Concatenation
         String fullname = fname + " " + lname;  // When line end then space will delete from memory because it not store in any variable (Temproary created memeory).
-        // System.out.println("Your name : " + fullname);
+        System.out.println("Your name : " + fullname);
         // System.out.println("Your name length : " + fullname.length());
         // for(int i=0; i<fullname.length();i++){
         //     System.out.println("Your name single Character : " + fullname.charAt(i));
@@ -30,11 +30,11 @@ class Strings{
         //     System.out.println("String are Not Equal.");
         // }
         // Method 2 : not recommanded (Same cases it failed on coding test cases)
-        // if(name1 == name2){
-        //     System.out.println("String are Equal.");
-        // }else{
-        //     System.out.println("String are Not Equal.");
-        // }
+        if(name1 == name2){
+            System.out.println("String are Equal.");
+        }else{
+            System.out.println("String are Not Equal.");
+        }
         // Example 2 : Better understanding for don't use == for compare two string
         // if(new String("Pks") == new String("Pks")){
         //     System.out.println("String are Equal.");
